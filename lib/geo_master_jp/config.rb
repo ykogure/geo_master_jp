db542c28e70f8551feefa31f69d7d84ee6ad76df
ykogure <renkin1008@gmail.com>
2019-08-13 01:53:11 +0900

    修正

--- lib/geo_master_jp/config.rb
+++ lib/geo_master_jp/config.rb
@@ -2,5 +2,9 @@ module GeoMasterJp
   class Config
     # Variables detail is writen in lib/generators/templates/geo_master_jp.rb.
     attr_accessor :alternative_class_names
+
+    def initialize
+      @alternative_class_names = {}
+    end
   end
 end
